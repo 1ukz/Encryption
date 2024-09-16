d82cef4106f9bc11be8e22e81c52735cac1ddc48
lucassaeta <lucassaeta2@gmail.com>
2024-09-16 20:36:32 +0200

    cbc

--- SymmetricCipher.java
+++ SymmetricCipher.java
@@ -1,67 +1,130 @@
-import java.io.FileDescriptor;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.security.NoSuchAlgorithmException;
-import javax.crypto.*;
+import javax.crypto.*;  
 import java.security.InvalidKeyException;
+import java.util.Arrays;  
 
 public class SymmetricCipher {
 
-	byte[] byteKey;
-	SymmetricEncryption s;
-	SymmetricEncryption d;
-	
-	// Initialization Vector (fixed)
-	
-	byte[] iv = new byte[] { (byte)49, (byte)50, (byte)51, (byte)52, (byte)53, (byte)54, 
-		(byte)55, (byte)56, (byte)57, (byte)48, (byte)49, (byte)50, (byte)51, (byte)52,
-		(byte)53, (byte)54};
+	//clave de cifrado
+    byte[] byteKey;
 
-    /*************************************************************************************/
-	/* Constructor method */
-    /*************************************************************************************/
-	public void SymmetricCipher() {
-	}
+    SymmetricEncryption s;
+    SymmetricEncryption d;
+    
+    //num de padding aniadido
+    int paddingLength;
 
-    /*************************************************************************************/
-	/* Method to encrypt using AES/CBC/PKCS5 */
-    /*************************************************************************************/
-	public byte[] encryptCBC (byte[] input, byte[] byteKey) throws Exception {
-		
-		byte[] ciphertext = null;	
-		
-		
-			// Generate the plaintext with padding
-			
-			
-	
-			// Generate the ciphertext
-			
-		
-		
-		return ciphertext;
-	}
-	
-	/*************************************************************************************/
-	/* Method to decrypt using AES/CBC/PKCS5 */
-    /*************************************************************************************/
-	
-	
-	public byte[] decryptCBC (byte[] input, byte[] byteKey) throws Exception {
-	
-		
-		byte [] finalplaintext = null;
-		
-			
-		// Generate the plaintext
-			
-		
-		// Eliminate the padding
-		
-			
-		
-		return finalplaintext;
-	}
-	
+    // iv inicializado como constante
+    static byte[] iv = new byte[] { 
+        (byte)49, (byte)50, (byte)51, (byte)52, (byte)53, (byte)54, 
+        (byte)55, (byte)56, (byte)57, (byte)48, (byte)49, (byte)50, 
+        (byte)51, (byte)52, (byte)53, (byte)54 
+    };
+
+    /* constructor method */
+    public SymmetricCipher() {
+        // vacio, ya que el iv ya esta iniciado estaticamente
+    }
+
+    /* method to add PKCS#5 padding */
+    private byte[] addPadding(byte[] input) {
+    	
+        if (input.length % 16 != 0) {       	
+	        // calcula del input, cuanto padding hara falta en el bloque necesario
+	        paddingLength = 16 - (input.length % 16); 
+	        // crea un nuevo array que contendra la longitud del input mas el padding, lo que ya hara que se pueda dividir perfectamente en bloques de 16
+	        byte[] padded = new byte[input.length + paddingLength];
+	        // copia los datos de un array al otro, es decir, los datos originales pero faltarian las posiciones donde va el padding por rellenar
+	        System.arraycopy(input, 0, padded, 0, input.length);
+	        // llena lo que queda del array con los datos copiados con el padding de PKCS#5
+	        for (int i = 0; i < paddingLength; i++) {
+	        	padded[input.length + i] = (byte) paddingLength;
+	        }   
+	        return padded;
+        }else {
+        	
+        	return input;
+        }
+    }
+
+    /* method to remove PKCS#5 padding */
+    private byte[] removePadding(byte[] input) {
+  		
+    		//creamos un nuevo array con el tamanio de el input descifrado con el padding menos el num de padding que hay
+    		byte[] decipheredText = new byte[input.length - this.paddingLength];
+    		//copiamos el array sin el padding
+    		decipheredText = Arrays.copyOfRange(input, 0, input.length - this.paddingLength);
+    		
+    		return decipheredText;
+    }
+
+    /* method to encrypt using AES/CBC/PKCS5 */
+    public byte[] encryptCBC(byte[] input, byte[] byteKey) throws Exception {
+        // crea una instancia de SymmetricEncryption con la clave dada
+        SymmetricEncryption aes = new SymmetricEncryption(byteKey);
+        
+        // aniade padding al texto en claro 
+        byte[] paddedInput = addPadding(input);
+
+        // crea un array para almacenar el ciphertext final
+        byte[] cipherText = new byte[paddedInput.length];
+
+        // el primer bloque se XOR-ea con el IV
+        byte[] block = iv;
+
+        // cifra bloque por bloque 
+        for (int i = 0; i < paddedInput.length; i += 16) {
+            // extrae un bloque de 16 bytes del input con el padding 
+            byte[] textToEncrypt = Arrays.copyOfRange(paddedInput, i, i + 16);
+            // realiza el XOR entre el bloque de texto en claro y el bloque anterior (o IV)
+            for (int j = 0; j < 16; j++) {
+            	textToEncrypt[j] ^= block[j];
+            }
+            // cifra el bloque utilizando AES
+            byte[] encryptedBlock = aes.encryptBlock(textToEncrypt);
+            // copia el bloque cifrado al array final de ciphertext
+            System.arraycopy(encryptedBlock, 0, cipherText, i, 16);
+            // Actualizar el bloque anterior con el bloque cifrado actual para la próxima iteración
+            block = encryptedBlock;
+        }
+        // devuelve el texto cifrado al completo
+        return cipherText;
+    }
+
+    /* Method to decrypt using AES/CBC/PKCS5 */
+    public byte[] decryptCBC(byte[] input, byte[] byteKey) throws Exception {
+        // crea una instancia de SymmetricEncryption con la clave dada
+        SymmetricEncryption aes = new SymmetricEncryption(byteKey);
+        // crea un array para almacenar el texto descifrado final
+        byte[] paddedDecipheredText = new byte[input.length];
+    	int numBlocks = input.length / 16;
+        byte[] lastBlock;
+        
+        // cifra bloque por bloque 
+        for (int i = input.length - 16; i >= 0; i -= 16) {
+            // extrae el bloque cifrado correspondiente de 16 bytes del input
+            byte[] textToDecrypt = Arrays.copyOfRange(input, i, i + 16);
+            // descifra el bloque utilizando AES
+            byte[] decryptedBlock = aes.decryptBlock(textToDecrypt);
+            
+            if (i == 0) {
+                // el ultimo bloque se XOR-ea con el IV
+                lastBlock = this.iv;
+            }else {
+            	// sino, pilla el anterior bloque cifrado del input
+            	lastBlock = Arrays.copyOfRange(input, i - 16, i);
+            }
+            // realiza el XOR entre el bloque de texto en claro y el bloque anterior (o IV)
+            for (int j = 0; j < 16; j++) {
+            	decryptedBlock[j] ^= lastBlock[j];
+            }
+            // copia el bloque cifrado al array final de ciphertext
+            System.arraycopy(decryptedBlock, 0, paddedDecipheredText, i, 16);
+        }
+        // devuelve el texto cifrado al completo
+        return removePadding(paddedDecipheredText);
+    }
+    	
+    	
 }
+       
+
--- Test_RSA.java
+++ Test_RSA.java
@@ -6,31 +6,71 @@ import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 import java.nio.charset.StandardCharsets;
 
 public class Test_RSA {
 	
 	public static void main(String[] args) throws Exception {
-		RSALibrary r = new RSALibrary();
-		r.generateKeys();
+		
+		/* ENCRIPTACION SIMETRICA */
+		
+		byte[] key = "31323334353637383930313233343536".getBytes();  // clave AES de 16 bytes
+        SymmetricCipher cipher = new SymmetricCipher();
+
+        
+        System.out.println(".....MESSAGE.....");
+		byte[] message = "Hola mundooo!".getBytes(); // Texto de prueba
+        
+        System.out.println("Plain text message: " + new String (message));
+        System.out.println("Length of plain text message: " + message.length);
+                
+        System.out.println("......ENCRYPTION.....");
+        byte[] encrypted = cipher.encryptCBC(message, key);
+        System.out.println("Encrypted: " + new String(encrypted));  
+        String encryptedBase64 = Base64.getEncoder().encodeToString(encrypted); 
+        System.out.println("Encrypted (Base64): " + encryptedBase64);
+        System.out.println("Length of encrypted text message:"  + encrypted.length);
+
+        System.out.println(".....DECRYPTION.....");
+        byte[] decrypted = cipher.decryptCBC(encrypted, key);
+        System.out.println("Decrypted: " + new String(decrypted));  
+        
+        System.out.println(".....MESSAGE.....");
+		byte[] message2 = "Hola esto es un mensaje mas largo para ver si funciona bien mi codigo!".getBytes(); // Texto de prueba
+        
+        System.out.println("Plain text message: " + new String (message2));
+        System.out.println("Length of plain text message: " + message2.length);
+                
+        System.out.println("......ENCRYPTION.....");
+        byte[] encrypted2 = cipher.encryptCBC(message2, key);
+        System.out.println("Encrypted: " + new String(encrypted2));  
+        String encryptedBase64_2 = Base64.getEncoder().encodeToString(encrypted2); 
+        System.out.println("Encrypted (Base64): " + encryptedBase64_2);
+        System.out.println("Length of encrypted text message:"  + encrypted2.length);
+
+        System.out.println(".....DECRYPTION.....");
+        byte[] decrypted2 = cipher.decryptCBC(encrypted2, key);
+        System.out.println("Decrypted: " + new String(decrypted2));  
+		
+		/* ENCRIPTACION ASIMETRICA */
+		//RSALibrary r = new RSALibrary();
+		//r.generateKeys();
 		
 		/* Read  public key*/
-		Path path = Paths.get("./public.key");
-		byte[] bytes = Files.readAllBytes(path);
+		//Path path = Paths.get("./public.key");
+		//byte[] bytes = Files.readAllBytes(path);
 		//Public key is stored in x509 format
-		X509EncodedKeySpec keyspec = new X509EncodedKeySpec(bytes);
-		KeyFactory keyfactory = KeyFactory.getInstance("RSA");
-		PublicKey publicKey = keyfactory.generatePublic(keyspec);
+		//X509EncodedKeySpec keyspec = new X509EncodedKeySpec(bytes);
+		//KeyFactory keyfactory = KeyFactory.getInstance("RSA");
+		//PublicKey publicKey = keyfactory.generatePublic(keyspec);
 		
 		/* Read private key */
-		path = Paths.get("./private.key");
-		byte[] bytes2 = Files.readAllBytes(path);
+		//path = Paths.get("./private.key");
+		//byte[] bytes2 = Files.readAllBytes(path);
 		//Private key is stored in PKCS8 format
-		PKCS8EncodedKeySpec keyspec2 = new PKCS8EncodedKeySpec(bytes2);
-		KeyFactory keyfactory2 = KeyFactory.getInstance("RSA");
-		PrivateKey privateKey = keyfactory2.generatePrivate(keyspec2);
-		
-
-......
-	
+		//PKCS8EncodedKeySpec keyspec2 = new PKCS8EncodedKeySpec(bytes2);
+		//KeyFactory keyfactory2 = KeyFactory.getInstance("RSA");
+		//PrivateKey privateKey = keyfactory2.generatePrivate(keyspec2);
+	}
 }
